53afc4fb0a10efc319e82d717690eeff6fc5b43f
Aaron Mendoza <aaronmendoza@Aarons-iMac.local>
2021-09-29 17:58:20 -0600

    NavBar added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import "semantic-ui-css/semantic.min.css";
 import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  useRouteMatch,
+  useParams,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Butts</header>
-    </div>
+    <Router>
+      <NavBar />
+    </Router>
   );
 }
 
---
+++ client/src/components/ResponsiveContainer/ResponsiveContainer.js
@@ -0,0 +1,157 @@
+import { createMedia } from "@artsy/fresnel";
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Segment,
+  Sidebar,
+  Visibility,
+} from "semantic-ui-react";
+
+const { MediaContextProvider, Media } = createMedia({
+  breakpoints: {
+    mobile: 0,
+    tablet: 768,
+    computer: 1024,
+  },
+});
+
+class DesktopContainer extends Component {
+  state = {};
+
+  hideFixedMenu = () => this.setState({ fixed: false });
+  showFixedMenu = () => this.setState({ fixed: true });
+
+  render() {
+    const { children } = this.props;
+    const { fixed } = this.state;
+
+    return (
+      <Media greaterThan="mobile">
+        <Visibility
+          once={false}
+          onBottomPassed={this.showFixedMenu}
+          onBottomPassedReverse={this.hideFixedMenu}
+        >
+          <Segment
+            inverted
+            textAlign="center"
+            style={{ padding: "1em 0em" }}
+            vertical
+          >
+            <Menu
+              fixed={fixed ? "top" : null}
+              inverted={!fixed}
+              pointing={!fixed}
+              secondary={!fixed}
+              size="large"
+            >
+              <Container>
+                <Menu.Item as="a">
+                  <Link to="/">Home</Link>
+                </Menu.Item>
+                <Menu.Item as="a">
+                  <Link to="/aboutUs">About Us</Link>
+                </Menu.Item>
+                <Menu.Item as="a">Invest</Menu.Item>
+                <Menu.Item as="a">Contact Us</Menu.Item>
+              </Container>
+            </Menu>
+          </Segment>
+        </Visibility>
+
+        {children}
+      </Media>
+    );
+  }
+}
+
+DesktopContainer.propTypes = {
+  children: PropTypes.node,
+};
+
+class MobileContainer extends Component {
+  state = {};
+
+  handleSidebarHide = () => this.setState({ sidebarOpened: false });
+
+  handleToggle = () => this.setState({ sidebarOpened: true });
+
+  render() {
+    const { children } = this.props;
+    const { sidebarOpened } = this.state;
+
+    return (
+      <Media as={Sidebar.Pushable} at="mobile">
+        <Sidebar.Pushable>
+          <Sidebar
+            as={Menu}
+            animation="overlay"
+            inverted
+            onHide={this.handleSidebarHide}
+            vertical
+            visible={sidebarOpened}
+          >
+            <Menu.Item as="a" active>
+              <Link to="/">Home</Link>
+            </Menu.Item>
+            <Menu.Item as="a">
+              <Link to="/aboutUs">About Us</Link>
+            </Menu.Item>
+            <Menu.Item as="a">Invest</Menu.Item>
+            <Menu.Item as="a">Contact Us</Menu.Item>
+          </Sidebar>
+
+          <Sidebar.Pusher dimmed={sidebarOpened}>
+            <Segment
+              inverted
+              textAlign="center"
+              style={{ padding: "1em 0em" }}
+              vertical
+            >
+              <Container>
+                <Menu inverted pointing secondary size="large">
+                  <Menu.Item onClick={this.handleToggle}>
+                    <Icon name="sidebar" />
+                  </Menu.Item>
+                </Menu>
+              </Container>
+            </Segment>
+
+            {children}
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </Media>
+    );
+  }
+}
+
+MobileContainer.propTypes = {
+  children: PropTypes.node,
+};
+
+const ResponsiveContainer = ({ children }) => (
+  /* Heads up!
+   * For large applications it may not be best option to put all page into these containers at
+   * they will be rendered twice for SSR.
+   */
+  <MediaContextProvider>
+    <DesktopContainer>{children}</DesktopContainer>
+    <MobileContainer>{children}</MobileContainer>
+  </MediaContextProvider>
+);
+
+ResponsiveContainer.propTypes = {
+  children: PropTypes.node,
+};
+
+export default ResponsiveContainer;
